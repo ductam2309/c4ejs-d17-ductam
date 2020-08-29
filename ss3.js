835e86dfa65ec710fc7df094215232f5ed49c28f
ductam2309 <nguyenvuductam2309@gmail.com>
2020-08-29 21:59:39 +0700

    abc

---
+++ ss3.js
@@ -0,0 +1,33 @@
+//1
+// let a = 5
+// let b = 6
+// temp = a
+// a=b
+// b= temp
+// console.log(a , b)
+//2
+// let a = 5
+// let b = 6
+// a = a + b
+// b = a-b
+// a = a-b
+// console.log(a , b)
+
+//3 
+// let s = ("helo beauty there")
+// let a = s.split (" ")
+// console.log(a)
+
+//4
+// 
+// let a = [4,5,7,-8]
+// console.log(...a)
+
+//review
+//4
+let n =prompt("hi there, what r u looking for(A,B,C)")
+let a = ["t-shirt","jean","socks"]
+console.log(a)
+for (x=0;x<a.length;x++){
+    console.log(a[x])
+}
